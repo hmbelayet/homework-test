2302745c42c296bc11cb42a53d17d679df0d7d06
MUHAMMAD HOSSAIN <belayet@MUHAMMADs-MacBook-Pro.local>
2018-11-25 20:28:01 -0500

    homeworktest

---
+++ src/homework/homework1.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class homework1 {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Belayet Hossain");
+		System.out.println("My student ID id 5946NY");
+		System.out.println("Address, 16410 84th ave, jamaica NY 11432");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author belayet
+ *
+ */
+module homework1 {
+}
